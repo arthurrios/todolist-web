338951a940739bd5445fa17fa269cd1006d1cef0
arthur <arthur.rios007@gmail.com>
2024-02-15 14:57:00 -0300

    feat: add tasks functions

--- src/App.tsx
+++ src/App.tsx
@@ -5,51 +5,108 @@ import { Header } from './components/Header'
 import { Input } from './components/Input'
 import { PlusCircle } from '@phosphor-icons/react'
 import { TaskCount } from './components/TaskCount'
-import { useState } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 import { EmptyContainer } from './components/EmptyContainer'
 import { Task, TaskType } from './components/Task'
 
-const tasksArray: TaskType[] = [
-  {
-    id: '1',
-    isDone: false,
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nostrum ullam molestiae nam libero. Est voluptate molestiae sapiente eos! Ad, itaque eum consequuntur assumenda tempora illo sapiente labore porro deserunt?',
-  },
-  {
-    id: '2',
-    isDone: true,
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ratione dolorum laudantium temporibus dolor excepturi veritatis repudiandae doloribus tenetur consequuntur voluptatum consectetur, aut corrupti nesciunt natus ab harum, magni aliquam.',
-  },
-]
-
 export function App() {
-  const [tasks, setTasks] = useState(tasksArray)
-  console.log(tasks.length)
+  const [tasks, setTasks] = useState<TaskType[]>([])
+  const [newTaskDescription, setNewTaskDescription] = useState('')
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('')
+    setNewTaskDescription(event.target.value)
+  }
+
+  function handleCreateNewTask(event: FormEvent) {
+    event?.preventDefault()
+    const newTask: TaskType = {
+      id: crypto.randomUUID(),
+      isDone: false,
+      description: newTaskDescription,
+    }
+    setTasks([...tasks, newTask])
+    setNewTaskDescription('')
+  }
+
+  function changeTaskStatus(taskToChangeId: string) {
+    const tasksWithNewCommentStatus = tasks.map((task) => {
+      if (task.id === taskToChangeId) {
+        return {
+          ...task,
+          isDone: !task.isDone,
+        }
+      }
+      return task
+    })
+
+    setTasks(tasksWithNewCommentStatus)
+  }
+
+  function deleteTask(taskToDeleteId: string) {
+    const tasksWithoutDeletedOne = tasks.filter((task) => {
+      return task.id !== taskToDeleteId
+    })
+    setTasks(tasksWithoutDeletedOne)
+  }
 
   const noTasksRegistered = tasks.length === 0
+  const noDescriptionProvided = newTaskDescription.length === 0
+
+  const tasksToDo = tasks.filter((task) => task.isDone === false)
+  const tasksDone = tasks.filter((task) => task.isDone)
 
   return (
     <div>
       <Header />
 
-      <div className={styles.inputWrapper}>
-        <Input placeholder="Add new task" />
-        <Button title="Create" icon={PlusCircle as IconNodeProps} />
-      </div>
+      <form onSubmit={handleCreateNewTask} className={styles.formWrapper}>
+        <Input
+          placeholder="Add new task"
+          onChange={handleNewTaskChange}
+          value={newTaskDescription}
+        />
+        <Button
+          type="submit"
+          disabled={noDescriptionProvided}
+          title="Create"
+          icon={PlusCircle as IconNodeProps}
+        />
+      </form>
 
       <main>
         <div className={styles.tasksCountWrapper}>
-          <TaskCount type="created" count={0} />
-          <TaskCount type="done" count={0} />
+          <TaskCount type="created" count={tasks.length} />
+          <TaskCount
+            type="done"
+            count={
+              tasks.length === 0 ? 0 : `${tasksDone.length} of ${tasks.length}`
+            }
+          />
         </div>
 
         {noTasksRegistered && <EmptyContainer />}
 
         <div className={styles.tasksContainer}>
-          {tasks.map((task) => {
-            return <Task key={task.id} task={task} />
+          {tasksToDo.map((task) => {
+            return (
+              <Task
+                key={task.id}
+                task={task}
+                onChangeTaskStatus={changeTaskStatus}
+                onDeleteTask={deleteTask}
+              />
+            )
+          })}
+          {tasksDone.map((task) => {
+            return (
+              <Task
+                key={task.id}
+                task={task}
+                onChangeTaskStatus={changeTaskStatus}
+                onDeleteTask={deleteTask}
+              />
+            )
           })}
         </div>
       </main>
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -9,24 +9,43 @@ export interface TaskType {
 
 interface TaskProps {
   task: TaskType
+  onChangeTaskStatus: (taskId: string) => void
+  onDeleteTask: (taskId: string) => void
 }
 
-export function Task({ task }: TaskProps) {
+export function Task({ task, onChangeTaskStatus, onDeleteTask }: TaskProps) {
+  function handleChangeStatus() {
+    onChangeTaskStatus(task.id)
+  }
+
+  function handleDeleteTask() {
+    onDeleteTask(task.id)
+  }
+
   return (
     <div
       className={
         task.isDone ? styles.containerTaskDone : styles.containerTaskCreated
       }
     >
+      <div>
+        <button
+          onClick={handleChangeStatus}
+          className={
+            task.isDone ? styles.checkedCircle : styles.uncheckedCircle
+          }
+        >
+          {task.isDone ? <Check weight="bold" size={11} /> : null}
+        </button>
+        <p className={task.isDone ? styles.descriptionTaskDone : undefined}>
+          {task.description}
+        </p>
+      </div>
       <button
-        className={task.isDone ? styles.checkedCircle : styles.uncheckedCircle}
+        onClick={handleDeleteTask}
+        className={styles.deleteButton}
+        title="Delete Task"
       >
-        <Check weight="bold" size={11} />
-      </button>
-      <p className={task.isDone ? styles.descriptionTaskDone : undefined}>
-        {task.description}
-      </p>
-      <button className={styles.deleteButton} title="Delete Task">
         <Trash weight="bold" size={16} />
       </button>
     </div>
